132decdf795d16b15dc31c20210fedfec7f70135
Dan Banfield <73112854+Dan-Banfield@users.noreply.github.com>
2023-11-02 16:14:48 +0000

    • Implement telemetry command.

--- Slim11/Features/Privacy/Telemetry.cs
+++ Slim11/Features/Privacy/Telemetry.cs
@@ -21,14 +21,23 @@ namespace Slim11.Features.Privacy
 
         public void Execute()
         {
-            Registry.SetValue(@"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\CompatTelRunner.exe", "Debugger", @"%windir%\System32\taskkill.exe", RegistryValueKind.String);
-
-            MessageBox.ShowInfo("Telemetry disabled successfully!");
+            try
+            {
+                Registry.SetValue(@"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\CompatTelRunner.exe", "Debugger", @"%windir%\System32\taskkill.exe", RegistryValueKind.String);
+                MessageBox.ShowInfo("Telemetry disabled successfully!");
+            }
+            catch { MessageBox.ShowError("Failed to disable telemetry."); }
         }
 
         public void Undo()
         {
-            MessageBox.ShowInfo("Telemetry enabled successfully.");
+            try
+            {
+                RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\CompatTelRunner.exe", true);
+                registryKey.DeleteValue("Debugger");
+                MessageBox.ShowInfo("Telemetry enabled successfully.");
+            }
+            catch { MessageBox.ShowError("Failed to enable telemetry."); }
         }
     }
 }
--- Slim11/Utilities/MessageBox.cs
+++ Slim11/Utilities/MessageBox.cs
@@ -3,5 +3,6 @@
     internal static class MessageBox
     {
         internal static void ShowInfo(string message) => System.Windows.Forms.MessageBox.Show(message, "Information", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Information);
+        internal static void ShowError(string message) => System.Windows.Forms.MessageBox.Show(message, "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
     }
 }
