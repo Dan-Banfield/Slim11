de19d0a0057a1c390c8c75d0e09f80a5dddbd378
Dan Banfield <73112854+Dan-Banfield@users.noreply.github.com>
2023-11-02 14:47:48 +0000

    • Update UI.

--- Slim11/Forms/MainForm.Designer.cs
+++ Slim11/Forms/MainForm.Designer.cs
@@ -28,30 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.fixTelemetryButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.restoreTelemetryButton = new System.Windows.Forms.Button();
+            this.tabControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // titleLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(37, 30);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 30);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Slim11";
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(26, 25);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(69, 30);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Slim11";
+            // 
+            // tabControl
+            // 
+            this.tabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Location = new System.Drawing.Point(27, 70);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(788, 379);
+            this.tabControl.TabIndex = 1;
+            this.tabControl.TabStop = false;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPage1.Location = new System.Drawing.Point(4, 39);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(780, 336);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Privacy";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // fixTelemetryButton
+            // 
+            this.fixTelemetryButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fixTelemetryButton.ForeColor = System.Drawing.Color.DarkGreen;
+            this.fixTelemetryButton.Location = new System.Drawing.Point(16, 35);
+            this.fixTelemetryButton.Name = "fixTelemetryButton";
+            this.fixTelemetryButton.Size = new System.Drawing.Size(78, 31);
+            this.fixTelemetryButton.TabIndex = 3;
+            this.fixTelemetryButton.TabStop = false;
+            this.fixTelemetryButton.Text = "Fix";
+            this.fixTelemetryButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.restoreTelemetryButton);
+            this.groupBox1.Controls.Add(this.fixTelemetryButton);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(24, 17);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(195, 84);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Telemetry";
+            // 
+            // restoreTelemetryButton
+            // 
+            this.restoreTelemetryButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restoreTelemetryButton.ForeColor = System.Drawing.Color.Crimson;
+            this.restoreTelemetryButton.Location = new System.Drawing.Point(100, 35);
+            this.restoreTelemetryButton.Name = "restoreTelemetryButton";
+            this.restoreTelemetryButton.Size = new System.Drawing.Size(78, 31);
+            this.restoreTelemetryButton.TabIndex = 4;
+            this.restoreTelemetryButton.TabStop = false;
+            this.restoreTelemetryButton.Text = "Restore";
+            this.restoreTelemetryButton.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(842, 478);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.titleLabel);
             this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Slim11";
+            this.tabControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,7 +132,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button fixTelemetryButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button restoreTelemetryButton;
     }
 }
 
